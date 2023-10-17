38885c27195114b2df5fd88fbf3ee31f0969af53
marcgray <marc@marcgarygray.com>
2023-10-16 22:16:59 -0700

    delete some boilerplate stuff

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,4 @@
 import React, { useEffect } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   useEffect(() => {
@@ -25,24 +23,7 @@ function App() {
     };
     void createStudent();
   });
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <p>hi!</p>;
 }
 
 export default App;
