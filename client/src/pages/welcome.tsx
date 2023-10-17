829973d0137972f49831548f68de123262c4176b
marcgray <marc@marcgarygray.com>
2023-10-16 23:23:43 -0700

    welcome

--- client/src/pages/welcome.tsx
+++ client/src/pages/welcome.tsx
@@ -1,3 +1,14 @@
+import { Link } from 'react-router-dom';
+
 export function Welcome() {
-  return <p>Welcome to the app!</p>;
+  return (
+    <>
+      <h1>Welcome to the ZoomInfo demo app.</h1>
+      <h2>
+        This app demonstrates understanding of React, TypeScript, Node, Express,
+        Prisma, and Jest.
+      </h2>
+      <Link to="/students">Click here</Link> to view the student list!
+    </>
+  );
 }
